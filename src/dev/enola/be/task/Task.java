0fe28b9e4655b61cf68c406e59cfd46ebfd94430
Michael Vorburger <mike@vorburger.ch>
2025-09-29 21:44:50 +0000

    Initial Task (WIP)

--- src/dev/enola/be/task/Task.java
+++ src/dev/enola/be/task/Task.java
@@ -1,4 +1,31 @@
 package dev.enola.be.task;
 
-public class Task {
+import java.time.Instant;
+import java.util.Optional;
+import java.util.Set;
+
+public interface Task<ID, I, O> {
+    // TODO extends Identifiable<TaskID>
+
+    /** 🆔 */
+    ID id();
+
+    I input();
+
+    CompletableFuture<O> output();
+
+    Status status();
+
+    void run() throws Exception;
+
+    Optional<Instant> startedAt();
+
+    Optional<Instant> endedAt();
+
+    /** Progress, as 0-100%. */
+    int progress();
+
+    void cancel();
+
+    Set<Task<?, ?, ?>> dependencies();
 }
