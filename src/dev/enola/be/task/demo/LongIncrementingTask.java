479ecfd7071d92a9beb452f654ad2e7b9f5745f0
Michael Vorburger <mike@vorburger.ch>
2025-10-04 18:48:29 +0200

    demo: LongIncrementingTask

--- src/dev/enola/be/task/demo/LongIncrementingTask.java
+++ src/dev/enola/be/task/demo/LongIncrementingTask.java
@@ -5,6 +5,7 @@ import dev.enola.be.task.demo.LongIncrementingTask.Input;
 import dev.enola.be.task.demo.LongIncrementingTask.Output;
 
 import java.time.Duration;
+import java.util.function.Consumer;
 
 public class LongIncrementingTask extends Task<Input, Output> {
 
@@ -12,13 +13,17 @@ public class LongIncrementingTask extends Task<Input, Output> {
 
     record Output(long result) {}
 
-    protected LongIncrementingTask(Input input) {
+    private final Consumer<Long> progressConsumer;
+
+    public LongIncrementingTask(Input input, Consumer<Long> progressConsumer) {
         super(input);
+        this.progressConsumer = progressConsumer;
     }
 
     @Override
     protected Output execute() throws Exception {
         for (long i = 0; i < input.max; i++) {
+            progressConsumer.accept(i);
             Thread.yield();
             if (Thread.currentThread().isInterrupted())
                 throw new InterruptedException("Task was interrupted");
@@ -35,5 +40,15 @@ public class LongIncrementingTask extends Task<Input, Output> {
         // TODO Stream counting
 
         return new Output(input.max);
+    }
+
+    public static void main(String[] args) {
+        // Count to 10000, with 1ms pause between each increment
+        var input = new Input(10000, Duration.ofMillis(1));
+        var task = new LongIncrementingTask(input, System.out::println);
+        try (var executor = new dev.enola.be.task.TaskExecutor()) {
+            var output = executor.await(task);
+            System.out.println("Output: " + output);
+        }
     }
 }
