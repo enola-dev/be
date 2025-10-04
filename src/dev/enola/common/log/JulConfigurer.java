518598df93fb2806080b514df040a39e63e2eada
Michael Vorburger <mike@vorburger.ch>
2025-10-05 01:09:42 +0200

    fix: Task end logging much better now

--- src/dev/enola/common/log/JulConfigurer.java
+++ src/dev/enola/common/log/JulConfigurer.java
@@ -5,22 +5,23 @@ import java.util.logging.Level;
 public final class JulConfigurer {
 
     public static void configureRootLogger() {
-        var logger = java.util.logging.Logger.getLogger("");
-        if (logger.getHandlers().length == 0) {
-            // Add a ConsoleHandler if none present yet
-            // (e.g. when no logging.properties is configured)
-            var handler = new java.util.logging.ConsoleHandler();
-            handler.setLevel(java.util.logging.Level.ALL);
-            logger.addHandler(handler);
+        String singleLineFormat = "[%1$tF %1$tT] [%4$-7s] %3$s - %5$s%6$s%n";
+        System.setProperty("java.util.logging.SimpleFormatter.format", singleLineFormat);
+        var rootLogger = java.util.logging.Logger.getLogger("");
+
+        // Remove all existing handlers (e.g. default ConsoleHandler)
+        for (var handler : rootLogger.getHandlers()) {
+            rootLogger.removeHandler(handler);
         }
-        // Remove other handlers (e.g. default ConsoleHandler
-        // added by JVM when no logging.properties is configured)
-        for (var handler : logger.getHandlers()) {
-            if (!(handler instanceof java.util.logging.ConsoleHandler)) { // should not happen?
-                logger.removeHandler(handler);
-            }
-        }
-        logger.setLevel(Level.ALL);
+
+        // Add a ConsoleHandler if none present yet
+        // (e.g. when no logging.properties is configured)
+        var handler = new java.util.logging.ConsoleHandler();
+        handler.setFormatter(new java.util.logging.SimpleFormatter());
+        handler.setLevel(java.util.logging.Level.ALL);
+        rootLogger.addHandler(handler);
+
+        rootLogger.setLevel(Level.ALL);
     }
 
     private JulConfigurer() {}
