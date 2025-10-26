04bfcbddd3108eef7c3df51949c8797a1f8a3721
Michael Vorburger <mike@vorburger.ch>
2025-10-27 00:08:25 +0100

    feat: javac -verbose etc.

--- src/ch/vorburger/jvmtools/JavaCompiler.java
+++ src/ch/vorburger/jvmtools/JavaCompiler.java
@@ -81,7 +81,6 @@ public class JavaCompiler implements Service<JavaCompiler.Input, Boolean> {
 
     @Override
     public Boolean invoke(Input input) throws Exception {
-        var err = input.stdIO().errWriter();
         var optionsList = new ArrayList<String>();
         if (input.options != null) {
             var options = input.options;
@@ -90,6 +89,27 @@ public class JavaCompiler implements Service<JavaCompiler.Input, Boolean> {
                 optionsList.add(options.outputDirectory.toString());
             }
         }
+
+        // Hard-coded, because we always want to see verbose output from javac.
+        // This does not mean that the DX is "chatty" though, as we control what later.
+        // It also shouldn't cause any notable performance issues (because of NonBlocking pumps).
+        optionsList.add("-verbose");
+
+        // Always include all debug info and the parameter names in compiled classes.
+        // The parameter names are useful for reflection-based frameworks etc. and generally
+        // recommended.
+        // It's intentionally hard-coded here to always be on, because we don't really see any
+        // use-case where one would want it off (unless class size is super critical, but then
+        // one would probably use any of the Code Shrinker or Optimizer tools afterwards anyway).
+        optionsList.add("-parameters");
+        optionsList.add("-g");
+
+        // Always show deprecation warnings, in the vain hope that developers will fix them.
+        // It's intentionally hard-coded here to always be on for now; but perhaps will be made a
+        // configurable option later.
+        optionsList.add("-deprecation");
+        optionsList.add("-Werror");
+
         var aptClasses = new ArrayList<String>(); // TODO
         var compilationUnits = input.sourcepath.getJavaFileObjects();
 
@@ -97,9 +117,10 @@ public class JavaCompiler implements Service<JavaCompiler.Input, Boolean> {
         var diagnosticListener = new StdIODiagnosticListener(input.stdIO());
         var fileManager = compiler.getStandardFileManager(diagnosticListener, null, null);
 
+        var out = input.stdIO().outWriter();
         var task =
                 compiler.getTask(
-                        err,
+                        out,
                         fileManager,
                         diagnosticListener,
                         optionsList,
